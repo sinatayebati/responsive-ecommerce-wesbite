90344864772a154e87e578b4f0208cbcbcbc7503
sinatayebati <sinatayebati@gmail.com>
2023-06-13 23:56:24 -0500

    updates commited

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 /*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId)
+    nav = document.getElementById(navId)
 
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('','')
 
 /*===== REMOVE MENU =====*/
 
