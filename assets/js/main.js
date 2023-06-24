4eeaef77bf80922c8a892dcfc8e57c971e5846d0
sinatayebati <sinatayebati@gmail.com>
2023-06-24 12:43:59 -0500

    updates committed

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ function scrollActive(){
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*='+ sectionsId + ']').classList.add('active')
         }else{
             document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
             
